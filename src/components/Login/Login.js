fe0aaf5c93bf48da8dab657cde97a144f0d7fa51
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-10-26 10:24:38 +0600

    added login form

---
+++ src/components/Login/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200 ">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                </div>
+                <form className="card flex-shrink-0 lg:w-[630px] max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                sp
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
