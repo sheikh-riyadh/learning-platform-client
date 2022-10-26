30e02136346e4d8d5371f808a2d85f9b6936c454
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-10-26 10:55:55 +0600

    added register form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Login></Login>
+      <Register></Register>
     </div>
   );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,21 +1,28 @@
 import React from 'react';
 
-const Login = () => {
+const Register = () => {
     const handleOnSubmit = (event) => {
         event.preventDefault()
         const form = event.target;
         const email = form.email.value
         const password = form.password.value;
-        console.log(email, password)
+        const name = form.name.value;
+        console.log(email, password, name)
     }
     return (
         <div className="hero min-h-screen bg-base-200 ">
             <div className="hero-content flex-col">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <h1 className="text-5xl font-bold">Register now</h1>
                 </div>
                 <form onSubmit={handleOnSubmit} className="card flex-shrink-0 lg:w-[630px] max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="name" name='name' placeholder="name" className="input input-bordered" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -28,13 +35,13 @@ const Login = () => {
                             </label>
                             <input type="password" name='password' placeholder="password" className="input input-bordered" />
                             <label className="label">
-                                <span>New here?
-                                    <a href="/" className="link link-hover ml-2">Register</a>
+                                <span>Already have an account?
+                                    <a href="/" className="link link-hover ml-2">Login</a>
                                 </span>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary">Register</button>
                         </div>
                     </div>
                 </form>
@@ -43,4 +50,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Register;
